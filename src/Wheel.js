62133aa55b773d963d5878e91065a1112f166537
Shiv <iamshivanand@users.noreply.github.com>
2020-07-29 17:27:37 +0530

    added a wheel

---
+++ src/Wheel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Wheel= (props) => {
+    return(
+        <div className="container">
+            <div className="outer-wheel">
+                <div className="inner-wheel">
+
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Wheel;
